813e18d955b102ffdf92793d05c2aac28d118713
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2015-10-31 03:17:32 +0000

    Fix unicorn config

--- config/unicorn/development.rb
+++ config/unicorn/development.rb
@@ -26,10 +26,10 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
 end
 
-after_fork do |server, worker
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
+after_fork do |server, worker|
+#  Signal.trap 'TERM' do
+#    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+#  end
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
